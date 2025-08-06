0d4b56c6ebdc6cc4051fe2d0f43dc77644dff05d
Sidhardha06 <sidhardhapatibandla06@gmail.com>
2025-08-06 14:59:09 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
   
   return (
     <>
-    <Router basename="/react-cicd">
+    <Router basename="/actions">
       <Routes>
         <Route path="/" element={<HomePage />}>
           <Route index element={<AboutUs />} />
